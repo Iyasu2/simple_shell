13075819c574097dabf962cd323eba729cbc9e84
iyasu2 <iyasuforreal@gmail.com>
2023-05-23 16:16:46 +0300

    with arguments

--- 0-main.c
+++ 0-main.c
@@ -2,7 +2,6 @@
 
 
 
-
 /**
 * main - the main shell code
 * @ac: number of arguments passed
@@ -16,6 +15,8 @@
 
 int main(int ac __attribute__((unused)), char **av)
 {
+char **commands = NULL;
+int status = 0;
 char *line = NULL;
 char *shell_name = NULL;
 size_t len = 0;
@@ -28,7 +29,22 @@ print("my_shell ", STDOUT_FILENO);
 if (getline(&line, &len, stdin) == -1)
 {
 free(line);
+exit(status);
 }
+commands = parse_arguments(line, "; ");
+for (i = 0;  commands[i] != NULL;  i++)
+{
+current_command = parse_arguments(commands[i], " ");
+if (current_command[0] == NULL)
+{
+	free(current_command);
+	break;
+}
+type_command = parse_input(current_command[0]);
+free(current_command);
+}
+free(commands);
 }
 free(line);
+return (status);
 }
